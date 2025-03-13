b5716d0dce50ab654f203c15f1e97beaf8fc6ecc
Arnold MBANZA <20ma304@esisalama.org>
2025-03-13 04:00:48 +0100

    correction de quelques erreurs au niveau de quelques fichiers

--- app/api/tasks/route.ts
+++ app/api/tasks/route.ts
@@ -1,23 +1,32 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextRequest) {
+// GET - Exemple d'une requête GET
+export async function GET() {
   return NextResponse.json(
     { message: "Voici une requête GET" },
     { status: 200 }
   );
 }
 
-export async function POST(req: NextRequest) {
-  const body = await req.json();
+// POST - Exemple d'une requête POST pour ajouter des données
+export async function POST() {
   return NextResponse.json(
     { message: "Les données sont enregistrées" },
     { status: 200 }
   );
 }
 
-export async function PUT(req: NextRequest) {
+// PUT - Exemple d'une requête PUT pour mettre à jour des données
+export async function PUT() {
   return NextResponse.json(
     { message: "Voici une requête PUT" },
     { status: 200 }
   );
+}
+
+// DELETE - Exemple d'une requête DELETE pour supprimer des données
+export async function DELETE(req: NextRequest) {
+  const id = req.nextUrl.searchParams.get('id');
+  // Ajoutez ici votre logique pour supprimer la tâche par ID
+  return NextResponse.json({ message: "Tâche supprimée" }, { status: 200 });
 }
